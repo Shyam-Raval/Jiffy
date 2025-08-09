18796fc8c6af55fdf708ac2f70d732b7cc0555d2
Shyam-Raval <109334692+Shyam-Raval@users.noreply.github.com>
2025-08-09 22:37:07 +0530

    first commit

---
+++ app/src/main/java/com/example/jiffy/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.jiffy
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.jiffy.screens.home.BottomNavigationBar
+import com.example.jiffy.screens.home.HomeScreen
+import com.example.jiffy.screens.home.MyTopAppBar
+import com.example.jiffy.ui.theme.JiffyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JiffyTheme {
+                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
+                    HomeScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JiffyTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/example/jiffy/screens/home/HomeScreen.kt
@@ -0,0 +1,32 @@
+package com.example.jiffy.screens.home
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
+
+
+@Composable
+fun HomeScreen() {
+    Scaffold(
+        topBar = { MyTopAppBar() },
+        bottomBar = { BottomNavigationBar() }
+    ) {paddingValues ->
+        Column(modifier = Modifier.fillMaxSize().padding(paddingValues)){
+            //search
+            val searchQuery = remember { mutableStateOf("") }
+            val focusManager = LocalFocusManager.current
+            //categoris
+            //featured
+           //featured products
+
+        }
+
+    }
+}
